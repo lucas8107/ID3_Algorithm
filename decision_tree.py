bf899cb52d53d88d203904d8b02e2247b6b23237
Lucas de Paula <luolcami@gmail.com>
2018-05-12 18:39:22 +0100

    final

--- decision_tree.py
+++ decision_tree.py
@@ -1,11 +1,45 @@
-from entropy import get_best_attribute
+from entropy import get_best_attribute, normalize_data
 import pandas as pd
 
+class Node:
+    def __init__(self, name, val_set):
+        self.name = name
+        self.val_set = val_set
+        
 def decision_tree(filename):
-    df = pd.read_csv(filename)
-    a = get_best_attribute(filename, df)
-    df.drop(a[0], axis=1, inplace=True)
-    print(a)
+    global df
+    df = pd.read_csv(filename, index_col=0)
+    normalize_data(df)
+    attrib_best = get_best_attribute(df)[0]
+    get_common()
+    ID3(df, list(df)[-1], attrib_best, 0)
+
+def get_common():
+    global common
+    global count
+    class_name = list(df)[-1]
+    common = set(df[class_name]).pop()
+    count = 0
+    for val in set(df[class_name]):
+        if count < df[df[class_name] == val][class_name].count():
+            count = df[df[class_name] == val][class_name].count()
+            common = val
+
+def ID3(dataframe, target_attrib, attribs, cnt):
+    root = Node(attribs, set(df[attribs]))
+    print("    "*cnt + "<{}>".format(attribs))
+    for val in root.val_set:
+        df_aux = dataframe[dataframe[attribs] == val]
+        a = set(df_aux[target_attrib])
+        if len(a) == 1:
+            print("    "*(cnt + 1) + "{}: {} ({})".format(val, a.pop(), df_aux[attribs].count()))
+        elif len(a) == 0:
+            print("    "*(cnt + 1) + "{}: {} ({})".format(val, common, count))
+        elif len(dataframe) > 1:
+            print("    "*(cnt + 1) + "{}: ".format(val))   
+            if len(list(df_aux.drop(attribs, axis=1))) == 1:
+                return
+            ID3(df_aux.drop(attribs, axis=1), target_attrib, get_best_attribute(df_aux.drop(attribs, axis=1))[0], cnt + 2)
 
 if __name__ == '__main__':
-    decision_tree('restaurant.csv')
+    decision_tree(input("File name: "))
--- entropy.py
+++ entropy.py
@@ -15,7 +15,7 @@ def entropy(attribute, df, class_name):
     clazz = dict(zip(clazz, (range(len(clazz)))))
 
     for i in range(aux):
-        count_matrix[attrib[df[attribute][i]]][clazz[df[class_name][i]]]+=1
+        count_matrix[attrib[df[attribute].iloc[i]]][clazz[df[class_name].iloc[i]]]+=1
 
     total_list = [0]*len(attrib)
 
@@ -29,11 +29,11 @@ def entropy(attribute, df, class_name):
 
     return sum(count_matrix)
 
-def get_best_attribute(filename, df):
+def get_best_attribute(df):
     columns_name = list(df)
     best = (None ,sys.maxsize)
     for title in df:
-        if title != columns_name[0] and title!=columns_name[-1]:
+        if title!=columns_name[-1]:
             aux = (title, entropy(title, df, columns_name[-1]))
             if best[1] > aux[1]:
                 best = aux
@@ -41,3 +41,27 @@ def get_best_attribute(filename, df):
         print("Error")
         return
     return best
+
+## The code below is for categorization of raw number data
+
+def num_int(dataframe, column):
+    min_ = dataframe[column].min()
+    max_ = dataframe[column].max()
+    R = max_ - min_
+    k = 1 + 3.222*np.log10(dataframe[column].count())
+    w = round(R/int(round(k)), 2)
+    for i in range(0, int(round(k))):
+        dataframe[column] = dataframe[column].apply(lambda x: fun(x, min_, w, i))
+    
+def fun(x, min_, w, i):
+    if type(x) is str:
+        return x
+    if min_ + w*i <= x <= min_ + w*(i + 1):
+        return '{} - {}'.format(round(min_ + w*i, 2), round(min_ + w*(i + 1), 2))
+    else:
+        return x
+
+def normalize_data(dataframe):
+    numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
+    for col in dataframe.select_dtypes(include=numerics):
+        num_int(dataframe, col)
