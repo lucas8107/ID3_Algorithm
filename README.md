f750f24afd6c43c10363cd38c6c51de4dabe7ef0
Lucas de Paula <luolcami@gmail.com>
2018-05-13 21:44:25 +0100

    final

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
 # assignment_4
 Code for the 4th AI assignment
 
+Para instalar a biblioteca necessária (pandas) basta executar:
+
+- sudo python3 setup.py install
+
+Para executar o programa:
+
+- python3 decision_tree.py (nome_do_arquivo_csv.csv)
+
+A árvore gerada aparecerá no shell e em um ficheiro com o nome
+"tree_nome_do_arquivo_csv.txt"
+
+
 | Aluno           | Numero UP     |
 | --------------- | ------------- |
 | Lucas de Paula  | 201608440     |
--- decision_tree.py
+++ decision_tree.py
@@ -1,5 +1,6 @@
 from entropy import get_best_attribute, normalize_data
 import pandas as pd
+import sys
 
 class Node:
     def __init__(self, name, val_set):
@@ -30,19 +31,30 @@ def get_common(val, attribs):
 def ID3(dataframe, target_attrib, attribs, cnt):
     root = Node(attribs, set(df[attribs]))
     print("    "*cnt + "<{}>".format(attribs))
+    file_.write("    "*cnt + "<{}>\n".format(attribs))
     for val in root.val_set:
         df_aux = dataframe[dataframe[attribs] == val]
         a = set(df_aux[target_attrib])
         if len(a) == 1:
-            print("    "*(cnt + 1) + "{}: {} ({})".format(val, a.pop(), df_aux[attribs].count()))
+            aux_1 = a.pop()
+            print("    "*(cnt + 1) + "{}: {} ({})".format(val, aux_1, df_aux[attribs].count()))
+            file_.write("    "*(cnt + 1) + "{}: {} ({})\n".format(val, aux_1, df_aux[attribs].count()))
         elif len(a) == 0:
             get_common(val, attribs)
             print("    "*(cnt + 1) + "{}: {} ({})".format(val, common, count))
+            file_.write("    "*(cnt + 1) + "{}: {} ({})\n".format(val, common, count))
         elif len(dataframe) > 1:
-            print("    "*(cnt + 1) + "{}: ".format(val))   
+            print("    "*(cnt + 1) + "{}: ".format(val))  
+            file_.write("    "*(cnt + 1) + "{}: \n".format(val))
             if len(list(df_aux.drop(attribs, axis=1))) == 1:
                 return
             ID3(df_aux.drop(attribs, axis=1), target_attrib, get_best_attribute(df_aux.drop(attribs, axis=1))[0], cnt + 2)
 
+file_ = None
+
 if __name__ == '__main__':
-    decision_tree(input("File name: "))
+    name = sys.argv[1]
+    file_name = name.split('.')[0]
+    file_ = open('tree_{}.txt'.format(file_name), 'w')
+    decision_tree(name)
+    file_.close()
